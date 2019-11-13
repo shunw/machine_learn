f3c641e20ddacfebee3333edc8af9d4d0e7bdd46
shunw <shunw.li@outlook.com>
2019-11-13 21:33:55 +0800

    update the table creation sql

--- src/dw/sql/test.sql
+++ src/dw/sql/test.sql
@@ -1,13 +1,27 @@
 USE tennis_data;
 
-SELECT round, 
-    @r:=CASE WHEN  @id <> tourney_id THEN 1 ELSE @r:= @r+1 END as round_count,
-    @id:= tourney_id as t_id
-FROM(
-    SELECT tourney_id, round, COUNT(round) as total
-    FROM tennis
-    GROUP BY round, tourney_id
-    HAVING tourney_id in ('2018-M020', '2018-0451', '2018-M001', '2016-0605', '2007-615')
-    ORDER BY tourney_id, total desc
-    LIMIT 20
-    ) totals, (SELECT @r:= 0, @id:=NULL)round_count;
+SELECT player_name, add_final.tourney_id, tourney_name, surface, draw_size, tourney_level, tourney_date, 
+        CASE WHEN final_winner_name is NULL THEN 0
+        ELSE 1 END as final_winner, 
+    r_t.round_total, w.winner_max_round, 
+    CASE WHEN w.winner_max_round is NULL THEN 0
+        ELSE w.winner_max_round / r_t.round_total * 100 END as win_percent, 
+    player_hand, player_age, player_ht, player_rank_points
+
+    FROM(
+    SELECT p.*, f.final_winner_name 
+    FROM player_info as p
+    LEFT JOIN final_win_info as f
+    ON f.final_winner_name = p.player_name and f.tourney_id = p.tourney_id
+    ) as add_final
+    
+    LEFT JOIN win_info as w
+    ON w.winner_name = add_final.player_name and w.tourney_id = add_final.tourney_id
+    
+    LEFT JOIN tourney_r_total as r_t
+    ON r_t.tourney_id = add_final.tourney_id
+    
+    ORDER BY final_winner_name DESC
+    
+    LIMIT 10 ;
+
--- src/dw/tennis_tables.py
+++ src/dw/tennis_tables.py
@@ -38,30 +38,31 @@ cursor = db1.cursor()
 #             FROM tennis
 #     ''')
 
-# get the tourney and its round information
-cursor.execute('''
-        CREATE TABLE tourney_r_inf
-        SELECT round, 
-            @r:=CASE WHEN  @id <> tourney_id THEN 1 ELSE @r:= @r+1 END as round_count,
-            @id:= tourney_id as t_id
-        FROM(
-            SELECT tourney_id, round, COUNT(round) as total
-            FROM tennis
-            GROUP BY round, tourney_id
-            ORDER BY tourney_id, total desc
+# # get the tourney and its round information
+# cursor.execute('''
+#         CREATE TABLE tourney_r_inf
+#         SELECT round, 
+#             @r:=CASE WHEN  @id <> tourney_id THEN 1 ELSE @r:= @r+1 END as round_count,
+#             @id:= tourney_id as t_id
+#         FROM(
+#             SELECT tourney_id, round, COUNT(round) as total
+#             FROM tennis
+#             GROUP BY round, tourney_id
+#             ORDER BY tourney_id, total desc
             
-            ) totals, (SELECT @r:= 0, @id:=NULL)round_count;
-        ''')
+#             ) totals, (SELECT @r:= 0, @id:=NULL)round_count;
+#         ''')
 
 # # tables = cursor.fetchall() 
 
 # # know the player win and lose information in a tourney
 # cursor.execute('''
 #     CREATE TABLE win_info
-#         SELECT 
-#             winner_name, COUNT(*) as winner_count, tourney_id
-#         FROM tennis
-#         GROUP BY tourney_id, winner_name
+#         SELECT ten.winner_name, MAX(tny.round_count) as winner_max_round, ten.tourney_id
+#         FROM tennis as ten
+#         LEFT JOIN tourney_r_inf as tny
+#         ON ten.tourney_id = tny.t_id and ten.round = tny.round
+#         GROUP BY ten.winner_name, ten.tourney_id;
 
 #     ''')
 
@@ -75,33 +76,42 @@ cursor.execute('''
 
 #     ''')
 
-# # create the table, player and his win/ lose count in a tourney
+# # get the tourney and it's round qty
 # cursor.execute('''
-# CREATE TABLE player_wl_tourney
-#     SELECT player_name, add_final.tourney_id, tourney_name, surface, draw_size, tourney_level, tourney_date, 
-#         CASE WHEN final_winner_name is NULL THEN 0
-#         ELSE 1 END as final_winner, 
-#     l.loser_count, w.winner_count, 
-#     CASE WHEN l.loser_count is NULL THEN 100
-#         WHEN w.winner_count is NULL THEN 0
-#         ELSE w.winner_count / (w.winner_count + l.loser_count) * 100 END as win_percent, 
-#     player_hand, player_age, player_ht, player_rank_points
+#     CREATE TABLE tourney_r_total
+#         SELECT t_id as tourney_id, MAX(round_count) as round_total
+#         FROM tourney_r_inf 
+#         GROUP BY t_id;
 
-#     FROM(
-#     SELECT p.*, f.final_winner_name 
-#     FROM player_info as p
-#     LEFT JOIN final_win_info as f
-#     ON f.final_winner_name = p.player_name and f.tourney_id = p.tourney_id
-#     ) as add_final
-    
-#     LEFT JOIN lose_info as l
-#     ON l.loser_name = add_final.player_name and l.tourney_id = add_final.tourney_id
-
-#     LEFT JOIN win_info as w
-#     ON w.winner_name = add_final.player_name and w.tourney_id = add_final.tourney_id
-    
-#     ORDER BY final_winner_name DESC
 #     ''')
+
+
+# create the table, player and his win/ lose count in a tourney
+cursor.execute('''
+CREATE TABLE player_wl_tourney
+    SELECT player_name, add_final.tourney_id, tourney_name, surface, draw_size, tourney_level, tourney_date, 
+        CASE WHEN final_winner_name is NULL THEN 0
+        ELSE 1 END as final_winner, 
+    r_t.round_total, w.winner_max_round, 
+    CASE WHEN w.winner_max_round is NULL THEN 0
+        ELSE w.winner_max_round / r_t.round_total * 100 END as win_percent, 
+    player_hand, player_age, player_ht, player_rank_points
+
+    FROM(
+    SELECT p.*, f.final_winner_name 
+    FROM player_info as p
+    LEFT JOIN final_win_info as f
+    ON f.final_winner_name = p.player_name and f.tourney_id = p.tourney_id
+    ) as add_final
+    
+    LEFT JOIN win_info as w
+    ON w.winner_name = add_final.player_name and w.tourney_id = add_final.tourney_id
+    
+    LEFT JOIN tourney_r_total as r_t
+    ON r_t.tourney_id = add_final.tourney_id
+    
+    ORDER BY final_winner_name DESC
+    ''')
 
 # cursor.execute('DROP TABLE final_win_info')
 
