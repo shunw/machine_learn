a22ebd8def8c4e18885cd409ca6ac536e2061fab
shunw <shunw.li@outlook.com>
2019-11-18 16:34:08 +0800

    get the mean from current back to some period

--- src/dw/tennis_data_handle.py
+++ src/dw/tennis_data_handle.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
 
 '''
 purpose: select query to get the main table and check the data
@@ -21,17 +22,16 @@ cursor = db1.cursor()
 
 
 sql_commnd = '''
-    SELECT player_name, win_percent, tourney_date 
+    SELECT *, win_percent * level_weight as wt_win_percent
     FROM player_wl_tourney
-    WHERE (lower(player_name) like '%raf%nad%' or lower(player_name) like '%rog%fed%') and tourney_date between '2017-01-01 00:00:00' and '2017-12-31 23:59:00' 
-    ORDER BY tourney_date
+    WHERE (lower(player_name) like '%raf%nad%' or lower(player_name) like '%rog%fed%') and tourney_date between '2015-01-01 00:00:00' and '2017-12-31 23:59:00' 
+    ORDER BY player_name, tourney_date
     '''
-
+# 
 df = pd.read_sql_query(sql = sql_commnd, con=db1)
 
-# cursor.execute('show columns in player_info')
-# tables = cursor.fetchall()
-# print (tables)
+db1.close()
+
 
 winner_name = 'winner_name'
 tourney_name = 'tourney_name'
@@ -42,12 +42,26 @@ surface = 'surface'
 draw_size = 'draw_size'
 loser_count = 'loser_count'
 win_percent = 'win_percent'
+rolling_score_180 = 'rolling_score_180'
+wt_win_percent = 'wt_win_percent'
 
-db1.close()
+
+
+# print (df.shape)
+def get_rolling_amount(grp, freq): 
+    return grp.rolling(freq, on='tourney_date')['wt_win_percent'].mean()
+
+df['rolling_score_180'] = np.transpose(df.groupby('player_name', as_index = False, group_keys = False).apply(get_rolling_amount, '180D'))
+# print (df.head())
+
+df['scaled_xxx'] = (df[wt_win_percent] - df[wt_win_percent].min())/ (df[wt_win_percent].max() - df[wt_win_percent].min())
 # print (df.head())
 fig, axes = plt.subplots(1, 1)
-axes.scatter(df.loc[df[player_name] == 'Rafael Nadal', tourney_date], df.loc[df[player_name] == 'Rafael Nadal', win_percent], c = 'navy')
-axes.scatter(df.loc[df[player_name] == 'Roger Federer', tourney_date], df.loc[df[player_name] == 'Roger Federer', win_percent], c = 'red')
+axes.scatter(df.loc[df[player_name] == 'Rafael Nadal', 'scaled_xxx'], df.loc[df[player_name] == 'Rafael Nadal', win_percent], c = 'navy')
+
+
+
+axes.scatter(df.loc[df[player_name] == 'Roger Federer', 'scaled_xxx'], df.loc[df[player_name] == 'Roger Federer', win_percent], c = 'red')
 plt.savefig('pic_output.png')
 
-# print (df.loc[df[player_name] == 'Rafael Nadal', tourney_date])
+# # print (df.loc[df[player_name] == 'Rafael Nadal', tourney_date])
