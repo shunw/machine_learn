f91c3b4893e2dd007bf652240bd5f8d945e8561c
shunw <shunw.li@outlook.com>
2019-11-20 16:21:19 +0800

    updated

--- src/dw/sql/test.sql
+++ src/dw/sql/test.sql
@@ -1,15 +1,7 @@
 USE tennis_data;
 
-SELECT winner_name, sum(winner_rank_points), date_format(tourney_date + interval 30 day, '%Y-%m-%d') as 'time_logged'
-FROM tennis
-GROUP BY  time_logged, winner_name
-HAVING winner_name like '%rog%fed%'
-ORDER BY time_logged
-LIMIT 10;
+select count(*) from(
+SELECT loser_name, l_ace, 'a'
 
-SELECT winner_name, winner_rank_points, tourney_date, date_format(tourney_date + interval 30 day, '%Y-%m-%d') as 'time_logged'
-FROM tennis
-
-HAVING winner_name like '%rog%fed%'
-ORDER BY tourney_date
-LIMIT 10;
+FROM tennis) as b
+;
