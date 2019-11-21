7f7eef3a647ea711dd60886ab14b40412a112d8d
shunw <shunw.li@outlook.com>
2019-11-21 20:13:45 +0800

    update sql

--- src/dw/sql/test.sql
+++ src/dw/sql/test.sql
@@ -1,7 +1,21 @@
 USE tennis_data;
 
 select count(*) from(
-SELECT loser_name, l_ace, 'a'
-
-FROM tennis) as b
+SELECT loser_name, tourney_id, l_ace, 'a'
+FROM tennis
+UNION
+SELECT winner_name, tourney_id, w_ace, 'b'
+FROM tennis
+) as b
 ;
+
+SELECT loser_name as 'player_name', tourney_date, tourney_id, l_ace as 'ace', 1 as 'w_or_l'
+FROM tennis
+WHERE loser_name like '%rog%fed%'
+-- ORDER BY tourney_date
+UNION
+SELECT winner_name as 'player_name', tourney_date, tourney_id, w_ace as 'ace', 0 as 'w_or_l'
+FROM tennis
+WHERE winner_name like '%rog%fed%'
+ORDER BY tourney_date
+LIMIT 10;
